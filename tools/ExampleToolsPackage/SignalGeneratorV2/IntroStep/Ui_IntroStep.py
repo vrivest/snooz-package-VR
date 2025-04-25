db3a4e3e96a5815216e84879bfdcfb108db8004b
vrivest <vincent.rivest.cnmtl@ssss.gouv.qc.ca>
2025-04-25 08:05:06 -0400

    ajout de l'intro step

---
+++ tools/ExampleToolsPackage/SignalGeneratorV2/IntroStep/Ui_IntroStep.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'Ui_IntroStep.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QSizePolicy, QSpacerItem, QVBoxLayout,
+    QWidget)
+import themes_rc
+
+class Ui_IntroStep(object):
+    def setupUi(self, IntroStep):
+        if not IntroStep.objectName():
+            IntroStep.setObjectName(u"IntroStep")
+        IntroStep.setStyleSheet(u"font: 12pt \"Roboto\";")
+        IntroStep.resize(730, 590)
+        self.verticalLayout = QVBoxLayout(IntroStep)
+        self.verticalLayout.setSpacing(25)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
+
+
+        self.retranslateUi(IntroStep)
+
+        QMetaObject.connectSlotsByName(IntroStep)
+    # setupUi
+
+    def retranslateUi(self, IntroStep):
+        IntroStep.setWindowTitle("")
+    # retranslateUi
+
