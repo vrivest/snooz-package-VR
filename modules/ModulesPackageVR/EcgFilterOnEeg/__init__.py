b5d5bc962b0afc449717ca6ab49aab2c4e753d6f
vrivest <vincent.rivest.cnmtl@ssss.gouv.qc.ca>
2025-04-29 13:51:31 -0400

    création de nouveaux packages

--- modules/ModulesPackageVR/EcgFilterOnEeg/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@ Valorisation Recherche HSCM, Societe en Commandite – 2025
-See the file LICENCE for full license details.
-"""
-from .EcgFilterOnEeg import EcgFilterOnEeg
-from .EcgFilterOnEegSettingsView import EcgFilterOnEegSettingsView
-from .EcgFilterOnEegResultsView import EcgFilterOnEegResultsView
-from .Ui_EcgFilterOnEegResultsView import Ui_EcgFilterOnEegResultsView
-from .Ui_EcgFilterOnEegSettingsView import Ui_EcgFilterOnEegSettingsView
