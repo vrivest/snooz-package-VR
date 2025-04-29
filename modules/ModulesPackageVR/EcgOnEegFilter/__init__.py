3da70ac91399e5aaf39093879c1162bf313297dd
vrivest <vincent.rivest.cnmtl@ssss.gouv.qc.ca>
2025-04-29 13:38:30 -0400

    suppression module et tool

--- modules/ModulesPackageVR/EcgOnEegFilter/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@ Valorisation Recherche HSCM, Societe en Commandite – 2025
-See the file LICENCE for full license details.
-"""
-from .EcgOnEegFilter import EcgOnEegFilter
-from .EcgOnEegFilterSettingsView import EcgOnEegFilterSettingsView
-from .EcgOnEegFilterResultsView import EcgOnEegFilterResultsView
-from .Ui_EcgOnEegFilterResultsView import Ui_EcgOnEegFilterResultsView
-from .Ui_EcgOnEegFilterSettingsView import Ui_EcgOnEegFilterSettingsView
