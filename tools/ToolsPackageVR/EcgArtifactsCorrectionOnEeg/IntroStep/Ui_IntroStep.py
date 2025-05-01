59bbae10b0bdffa56ffae3931b22ecd6f688f092
vrivest <vincent.rivest.cnmtl@ssss.gouv.qc.ca>
2025-05-01 10:29:57 -0400

    modif ui intro step

--- tools/ToolsPackageVR/EcgArtifactsCorrectionOnEeg/IntroStep/Ui_IntroStep.py
+++ tools/ToolsPackageVR/EcgArtifactsCorrectionOnEeg/IntroStep/Ui_IntroStep.py
@@ -15,36 +15,18 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QSizePolicy, QSpacerItem,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QSizePolicy, QWidget)
 import themes_rc
 
 class Ui_IntroStep(object):
     def setupUi(self, IntroStep):
         if not IntroStep.objectName():
             IntroStep.setObjectName(u"IntroStep")
-        IntroStep.resize(730, 590)
+        IntroStep.resize(724, 577)
         IntroStep.setStyleSheet(u"font: 12pt \"Roboto\";")
-        self.verticalLayout = QVBoxLayout(IntroStep)
-        self.verticalLayout.setSpacing(25)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.verticalLayout.addItem(self.horizontalSpacer)
-
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.verticalLayout.addItem(self.horizontalSpacer_2)
-
         self.label = QLabel(IntroStep)
         self.label.setObjectName(u"label")
-
-        self.verticalLayout.addWidget(self.label)
-
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.verticalLayout.addItem(self.verticalSpacer)
-
+        self.label.setGeometry(QRect(20, 20, 321, 20))
 
         self.retranslateUi(IntroStep)
 
@@ -53,6 +35,6 @@ class Ui_IntroStep(object):
 
     def retranslateUi(self, IntroStep):
         IntroStep.setWindowTitle("")
-        self.label.setText(QCoreApplication.translate("IntroStep", u"Tool to filter EEG signals from ECG artifacts VR", None))
+        self.label.setText(QCoreApplication.translate("IntroStep", u"Tool to filter EEG signals from ECG artifacts", None))
     # retranslateUi
 
